45a502cb855c73e59adbba479a4b29dd4dc3f10c
joaquim_og@hotmail.com <virgem89>
2020-04-24 15:37:20 -0300

    fix some bugs caused by cleaning initial code

--- app/src/main/java/com/joaquim/chucknorrisio/MainActivity.java
+++ app/src/main/java/com/joaquim/chucknorrisio/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.support.design.widget.AppBarLayout;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
